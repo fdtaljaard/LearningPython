74547ada18d15cc3d1690a5f1083f6a4d393e79e
fdtaljaard <francoist@cradle.co.za>
2019-07-23 10:27:29 +0200

    Update Inheritance.py

--- src/OOP/Inheritance.py
+++ src/OOP/Inheritance.py
@@ -9,16 +9,31 @@ class BaseClass():
 	def BaseMethod():
 		print("BaseClass method called")
 
-class DerivedClass(BaseClass):
+class BaseClass1():
+	
+	def __init_subclass__(self):
+		print("BaseClass1 __init_subclass__")
+
+	def __init__(self):
+		print("BaseClass1 __init__")
+
+	def BaseMethod():
+		print("BaseClass1 method called")
+		
+# can inherint more than one class, take note it will 
+# only call the first class __init_subclass__ ? 
+class DerivedClass(BaseClass1, BaseClass):
 	
 	def __init_subclass__(self):
 		print("DerivedClass __init_subclass__")
 
 	def __init__(self):
-		super().__init__()
+		# if you want to run base __init__
+		super().__init__() 
 		print("DerivedClass __init__")
 
 	def BaseMethod(self):
+		# if you want to call base BaseMethod
 		BaseClass.BaseMethod()
 		print("DerivedClass method called")
 
