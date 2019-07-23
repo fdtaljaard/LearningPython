ce64c5dc873819c44214d7785f2a1efdd28da13f
fdtaljaard <francoist@cradle.co.za>
2019-07-23 10:55:11 +0200

    Update Inheritance.py

--- src/OOP/Inheritance.py
+++ src/OOP/Inheritance.py
@@ -6,8 +6,15 @@ class BaseClass():
 	def __init__(self):
 		print("BaseClass __init__")
 
-	def BaseMethod():
-		print("BaseClass method called")
+	def Method1(self):
+		print("BaseClass Method1 called")
+
+	def Method2(self):
+		print("BaseClass Method2 called")
+
+	def Method3(self):
+		# override 
+		print("BaseClass Method3 called")
 
 class BaseClass1():
 	
@@ -17,9 +24,12 @@ class BaseClass1():
 	def __init__(self):
 		print("BaseClass1 __init__")
 
-	def BaseMethod():
-		print("BaseClass1 method called")
-		
+	def Method1(self):
+		print("BaseClass1 Method1 called")
+
+	def Method2(self):
+		print("BaseClass1 Method2 called")
+
 # can inherint more than one class, take note it will 
 # only call the first class __init_subclass__ ? 
 class DerivedClass(BaseClass1, BaseClass):
@@ -32,11 +42,26 @@ class DerivedClass(BaseClass1, BaseClass):
 		super().__init__() 
 		print("DerivedClass __init__")
 
-	def BaseMethod(self):
-		# if you want to call base BaseMethod
-		BaseClass.BaseMethod()
-		print("DerivedClass method called")
+	def Method1(self):
+		# override 
+		print("DerivedClass Method1 called")
+
+	def Method2(self):
+		BaseClass.Method2(self)
+		BaseClass1.Method2(self)
+		print("DerivedClass Method2 called")
 
 
 dc = DerivedClass()
-dc.BaseMethod()
+# call DerivedClass Method3, inherit from BaseClass
+dc.Method3()
+
+# call DerivedClass Method1, almost like override
+dc.Method1()
+
+# call DerivedClass Method2
+dc.Method2()
+# is the instance of class ?type TypeOf?
+print(isinstance(dc, BaseClass))
+print(isinstance(dc, BaseClass1))
+print(dir(dc))
